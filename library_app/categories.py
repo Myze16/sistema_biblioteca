d9408929eff62b030585f43f29593b81f01a8df7
Mizael Pardal <mizaellucaspardal@hotmail.com>
2022-05-08 10:42:47 -0300

    feat: add Topic class

---
+++ library_app/categories.py
@@ -0,0 +1,29 @@
+class Topic:
+    def __init__(self, name, descripion, subject):
+        self._name = name
+        self._description = descripion
+        self._subject = subject
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    @property
+    def description(self):
+        return self._description
+    
+    @description.setter
+    def description(self, description):
+        self._description = description
+
+    @property
+    def subject(self):
+        return self._subject
+    
+    @subject.setter
+    def subject(self, subject):
+        self._subject = subject

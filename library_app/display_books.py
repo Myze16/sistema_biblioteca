72475760fb34e7ff5fe33c6c7f22862b298d5041
Junior <86266735+KolzzII@users.noreply.github.com>
2022-05-17 19:04:08 -0300

    feat: book menu

--- library_app/display_books.py
+++ library_app/display_books.py
@@ -1,44 +1,62 @@
+from book import Book
+from categories import Topic
+from database import book_dict
+
 class BookDisplay:
-    def register_book_topic():
-        option = input('''
- ________________________________
-|                                |
-|         REGISTER BOOK          |
-|           AND TOPIC            |
-|                         _____  |
-|  1- Register Book      |    || |
-|  2- Register Exemplary |Book|| |
-|  3- Register Topic     |____|| |
-|________________________________|
-''')
-        match option:
-            case "1":
-                pass
-            case "2":
-                pass
-            case "3":
-                pass
-            case _:
-                print("Please enter a valid option")
+    def register_book_topic(self):
+        while True:
+            option = input('''
+     ________________________________
+    |                                |
+    |         REGISTER BOOK          |
+    |           AND TOPIC            |
+    |                         _____  |
+    |  1- Register Book      |    || |
+    |  2- Register Exemplary |Book|| |
+    |  3- Register Topic     |____|| |
+    |  4- Consult Book               |
+    |  5- Return                     |
+    |________________________________|
+    ''')
+            match option:
+                case "1":
+                    book = Book.get_info()
+                    print(f"Book {book.title} registered")
+                    book_dict[book.title] = book
+                case "2":
+                    pass
+                case "3":
+                    topic = Topic.get_info()
+                    print(f"Topic {topic.name} registered")
+                case "4":
+                    self.consult_book()
+                case "5":
+                    break
+                case _:
+                    print("Please enter a valid option")
 
 
-    def consult_book():
-        option = print('''
- ____________________________
-|                            |
-|       CONUSLT BOOK         |
-|                         _  |
-|   1- Search by name    (_) |
-|   3- Search by topic   /   |
-|   2- Search by Author      |
-|____________________________|
-''')
-        match option:
-            case "1":
-                pass
-            case "2":
-                pass
-            case "3":
-                pass
-            case _:
-                print("Please enter a valid option")
+    def consult_book(self):
+        while True:
+            option = input('''
+     ____________________________
+    |                            |
+    |       CONSULT BOOK         |
+    |                         _  |
+    |   1- Search by name    (_) |
+    |   2- Search by topic   /   |
+    |   3- Search by Author      |
+    |   4- Return                |
+    |____________________________|
+    ''')
+            match option:
+                case "1":
+                    pass
+                case "2":
+                    pass
+                case "3":
+                    pass
+                case "4":
+                    break
+                case _:
+                    print("Please enter a valid option")
--- library_app/main.py
+++ library_app/main.py
@@ -1,27 +1,10 @@
-from book import Book, Examplary
 from categories import Topic
+from display_books import BookDisplay
 
 def main():
+    display_book = BookDisplay()
     while True:
-        option = input("""
-    1 - Register a book
-    2 - Register a examplary
-    3 - Register a topic
-    4 - Return
-
-    Enter the desired option: """)
-        match option:
-            case "1":
-                book = Book.get_info()
-                print(f"Book {book.title} created")
-            case "2":
-                pass
-            case "3":
-                topic = Topic.get_info()
-                print(f"Topic {topic.name} created")
-            case _:
-                print("Please enter a valid option")
-
-
+        display_book.register_book_topic()
+        
 if __name__ == "__main__":
     main()

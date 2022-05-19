140715459893c678fb7007c23866aec6b078fab9
Gabriel Soares gomes <dev.gabrielsoares221@gmail.com>
2022-05-19 16:50:09 -0300

    feat: Search by author

--- library_app/display_books.py
+++ library_app/display_books.py
@@ -72,10 +72,18 @@ class BookDisplay:
                             print("-="*20)
                             print(f"Title: {book.title} \nAuthor: {book.author} \nExamplaries: {book.examplary} \nEdition: {book.edition} \nTopic: {book.topic.name} \nPublishing Company: {book.publishing_company} \nYear: {book.year}")
                     if not book_found:
-                        print("No books with tha topic were found!")
+                        print("No books with that topic were found!")
 
                 case "3":
-                    pass
+                    search_book_author = input("Enter boom author: ")
+                    book_found = False
+                    for book in book_dict.values():
+                        if book.author.upper() == search_book_author.upper():
+                            book_found = True
+                            print("-="*20)
+                            print(f"Title: {book.title} \nAuthor: {book.author} \nExamplaries: {book.examplary} \nEdition: {book.edition} \nTopic: {book.topic.name} \nPublishing Company: {book.publishing_company} \nYear: {book.year}")
+                        if not book_found:
+                            print("No books with that author were found!")
                 case "4":
                     break
                 case _:
